c3cbe9209065d4322e7b06931d547fe67c003614
Murod <murodnazarov150@gmail.com>
2022-04-30 20:15:01 +0500

    Start App

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <nav-bar/>
+      <Hero/>
+      <exchangele-list/>
+      <pagination/>
+  </div>
+</template>
+
+<script>
+  import NavBar from '@/components/NavBar.vue'
+  import Hero from '@/components/Hero.vue'
+import ExchangeleList from './components/ExchangeleList.vue'
+import Pagination from './components/Pagination.vue'
+export default {
+  name: 'App',
+  components:{
+    NavBar,
+    Hero,
+    ExchangeleList,
+    Pagination
+  }
+}
+</script>
+
+<style lang="scss">
+    @import './assets/styles/variables.scss';
+    @import '~bulma/bulma.sass';
+    @import './assets/styles/main.scss'; 
+</style>
